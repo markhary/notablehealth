8c8a02aeada0aa6be40b9110ce7675dda626b0e9
Mark Hary <44713017+markhary@users.noreply.github.com>
2018-11-15 00:44:50 -0800

    Fixed bug where I was using old doctorIDs in config.database.js.

--- config.database.js
+++ config.database.js
@@ -1,4 +1,8 @@
 'use strict';
+const timeoutPromise = (timeout) => {
+return new Promise((resolve) => setTimeout(resolve, timeout));
+};
+
 (async () => {
     //
     // Put the database into the default state
@@ -27,21 +31,18 @@
     // Populate the doctor collection
     await db.get('doctors').insert([
         {
-            "_id": "5becafede2ac7cdc2c68f373",
             "name": {
                 "first": "Julius",
                 "last": "Hibbert"
             }
         },
         {
-            "_id": "5becaffce2ac7cdc2c68f377",
             "name": {
                 "first": "Algernop",
                 "last": "Krieger"
             }
         },
         {
-            "_id": "5becb007e2ac7cdc2c68f37b",
             "name": {
                 "first": "Nick",
                 "last": "Riviera"
@@ -49,86 +50,89 @@
         }
     ]);
 
-    // Populate the appointments collection
-    await db.get('appointments').insert([
-        {
-            "_id": "5becbbb5e2ac7cdc2c68f5f3",
-            "doctorID": "5becafede2ac7cdc2c68f373",
-            "name": {
-                "first": "Clark",
-                "last": "Kent"
+    await db.get('doctors').find({}, {sort:{"name.last": 1}}, function(err, doctors) {
+        const drHibbertID = doctors[0]._id;
+        const drKriegerID = doctors[1]._id;
+
+        console.log("Dr Julius has ID " + drHibbertID);
+        console.log("Dr Krieger has ID " + drKriegerID);
+
+        // Populate the appointments collection
+        db.get('appointments').insert([
+            {
+                "doctorID": drHibbertID,
+                "name": {
+                    "first": "Clark",
+                    "last": "Kent"
+                },
+                "date": "20180617",
+                "time": "08:00:00",
+                "type": "New Patient"
             },
-            "date": "20180617",
-            "time": "08:00:00",
-            "type": "New Patient"
-        },
-        {
-            "_id": "5becbbdfe2ac7cdc2c68f60d",
-            "doctorID": "5becafede2ac7cdc2c68f373",
-            "name": {
-                "first": "Lois",
-                "last": "Lane"
+            {
+                "doctorID": drHibbertID,
+                "name": {
+                    "first": "Lois",
+                    "last": "Lane"
+                },
+                "date": "20180617",
+                "time": "08:00:00",
+                "type": "Follow-up"
             },
-            "date": "20180617",
-            "time": "08:00:00",
-            "type": "Follow-up"
-        },
-        {
-            "_id": "5becbbf6e2ac7cdc2c68f616",
-            "doctorID": "5becafede2ac7cdc2c68f373",
-            "name": {
-                "first": "Lex",
-                "last": "Luthor"
+            {
+                "doctorID": drHibbertID,
+                "name": {
+                    "first": "Lex",
+                    "last": "Luthor"
+                },
+                "date": "20180617",
+                "time": "08:00:00",
+                "type": "New Patient"
             },
-            "date": "20180617",
-            "time": "08:00:00",
-            "type": "New Patient"
-        },
-        {
-            "_id": "5becbc1ae2ac7cdc2c68f630",
-            "doctorID": "5becafede2ac7cdc2c68f373",
-            "name": {
-                "first": "Lex",
-                "last": "Luthor"
+            {
+                "doctorID": drHibbertID,
+                "name": {
+                    "first": "Lex",
+                    "last": "Luthor"
+                },
+                "date": "20180617",
+                "time": "09:00:00",
+                "type": "Follow-up"
             },
-            "date": "20180617",
-            "time": "09:00:00",
-            "type": "Follow-up"
-        },
-        {
-            "_id": "5becbc24e2ac7cdc2c68f636",
-            "doctorID": "5becafede2ac7cdc2c68f373",
-            "name": {
-                "first": "Lex",
-                "last": "Luthor"
+            {
+                "doctorID": drHibbertID,
+                "name": {
+                    "first": "Lex",
+                    "last": "Luthor"
+                },
+                "date": "20180618",
+                "time": "09:00:00",
+                "type": "Follow-up"
             },
-            "date": "20180618",
-            "time": "09:00:00",
-            "type": "Follow-up"
-        },
-        {
-            "_id": "5becd399e2ac7cdc2c68fd3b",
-            "doctorID": "5becaffce2ac7cdc2c68f377",
-            "name": {
-                "first": "Clark",
-                "last": "Kent"
+            {
+                "doctorID": drKriegerID,
+                "name": {
+                    "first": "Clark",
+                    "last": "Kent"
+                },
+                "date": "20180617",
+                "time": "08:00:00",
+                "type": "New Patient"
             },
-            "date": "20180617",
-            "time": "08:00:00",
-            "type": "New Patient"
-        },
-        {
-            "_id": "5bed01a7e2ac7cdc2c6902b9",
-            "doctorID": "5becafede2ac7cdc2c68f373",
-            "name": {
-                "first": "Lex",
-                "last": "Luthor"
-            },
-            "date": "20180617",
-            "time": "07:00:00",
-            "type": "Follow-up"
-        }
-    ]);
+            {
+                "doctorID": drHibbertID,
+                "name": {
+                    "first": "Lex",
+                    "last": "Luthor"
+                },
+                "date": "20180617",
+                "time": "07:00:00",
+                "type": "Follow-up"
+            }
+        ]);
+    });
+
+    await timeoutPromise(2000);
 
     await db.get('doctors').find({}, function(err, res) {
         console.log("Populated doctors: " + JSON.stringify(res));
@@ -137,6 +141,7 @@
     await db.get('appointments').find({}, function(err, res) {
         console.log("Populated appointments: " + JSON.stringify(res));
     });
+
     await db.close();
 
 })().catch(err=> {
